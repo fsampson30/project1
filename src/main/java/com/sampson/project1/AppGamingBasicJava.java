8986897e8451098ee18f897599784efecaca147d
fsampson30 <fsampson30@gmail.com>
2023-10-17 08:06:30 -0300

    Created class to represent a new game.

--- src/main/java/com/sampson/project1/AppGamingBasicJava.java
+++ src/main/java/com/sampson/project1/AppGamingBasicJava.java
@@ -2,12 +2,14 @@ package com.sampson.project1;
 
 import com.sampson.project1.game.GameRunner;
 import com.sampson.project1.game.MarioGame;
+import com.sampson.project1.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
     public static void main(String[] args){
 
         var marioGame = new MarioGame();
+        var superContraGame = new SuperContraGame();
         var gameRunner = new GameRunner(marioGame);
         gameRunner.run();
 
