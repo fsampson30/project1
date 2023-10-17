50fe7cea0ceacc56f7e04fa2e2a455c057fba2c7
fsampson30 <fsampson30@gmail.com>
2023-10-17 07:37:00 -0300

    Initial commit

---
+++ src/test/java/com/sampson/project1/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
