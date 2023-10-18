6bbcc9ba62553828fe5dd6745d224a679d078756
fsampson30 <fsampson30@gmail.com>
2023-10-18 07:23:42 -0300

    Created PacManGame.java to represent a new game.

--- src/main/java/com/sampson/project1/AppGamingBasicJava.java
+++ src/main/java/com/sampson/project1/AppGamingBasicJava.java
@@ -2,6 +2,7 @@ package com.sampson.project1;
 
 import com.sampson.project1.game.GameRunner;
 import com.sampson.project1.game.MarioGame;
+import com.sampson.project1.game.PacManGame;
 import com.sampson.project1.game.SuperContraGame;
 
 public class AppGamingBasicJava {
@@ -10,7 +11,8 @@ public class AppGamingBasicJava {
 
         var marioGame = new MarioGame();
         var superContraGame = new SuperContraGame();
-        var gameRunner = new GameRunner(marioGame);
+        var pacman = new PacManGame();
+        var gameRunner = new GameRunner(pacman);
         gameRunner.run();
 
     }
---
+++ src/main/java/com/sampson/project1/game/PacManGame.java
@@ -0,0 +1,26 @@
+package com.sampson.project1.game;
+
+public class PacManGame implements GamingConsole{
+    @Override
+    public void up() {
+        System.out.println("Up in the maze");
+    }
+
+    @Override
+    public void down() {
+        System.out.println("Down in the maze");
+
+    }
+
+    @Override
+    public void left() {
+        System.out.println("Left in the maze");
+
+    }
+
+    @Override
+    public void right() {
+        System.out.println("Right in the maze");
+
+    }
+}
