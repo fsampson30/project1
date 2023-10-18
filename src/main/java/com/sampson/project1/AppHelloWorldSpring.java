2652bd3e9eaa3ec6b146ed0f6ee6c93596486cfc
fsampson30 <fsampson30@gmail.com>
2023-10-18 07:55:07 -0300

    Created new Spring configuration class and uses Beans.

---
+++ src/main/java/com/sampson/project1/AppHelloWorldSpring.java
@@ -0,0 +1,19 @@
+package com.sampson.project1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppHelloWorldSpring {
+
+    public static void main(String[] args){
+
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("adress"));
+
+
+
+    }
+}
---
+++ src/main/java/com/sampson/project1/HelloWorldConfiguration.java
@@ -0,0 +1,31 @@
+package com.sampson.project1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+record Person(String name, int age) {} ;
+record Address(String firstLine, String city) {} ;
+@Configuration
+public class HelloWorldConfiguration {
+
+    @Bean
+    public String name() {
+        return "Flavio";
+    }
+
+    @Bean
+    public int age() {
+        return 15;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Fernanda",12);
+    }
+
+    @Bean
+    public Address adress() {
+        return new Address("A Street, number 1, ","HillValley");
+    }
+
+}
