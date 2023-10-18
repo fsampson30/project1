aac71b10b356ddd759ffdd223816243e3acb568e
fsampson30 <fsampson30@gmail.com>
2023-10-18 08:09:10 -0300

    Added new Beans.

--- src/main/java/com/sampson/project1/HelloWorldConfiguration.java
+++ src/main/java/com/sampson/project1/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.sampson.project1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {} ;
+record Person(String name, int age, Address address) {} ;
 record Address(String firstLine, String city) {} ;
 @Configuration
 public class HelloWorldConfiguration {
@@ -20,7 +20,17 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("Fernanda",12);
+        return new Person("Fernanda",12, adress());
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(),age(), adress());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2) {
+        return new Person(name,age, address2);
     }
 
     @Bean(name = "address2")
