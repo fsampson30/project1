58dbbfc667719006b45bd63679ec99112a109845
fsampson30 <fsampson30@gmail.com>
2023-10-20 07:25:14 -0300

    Refactored some code to use @Component and @ComponentScan

--- src/main/java/com/sampson/project1/game/GameRunner.java
+++ src/main/java/com/sampson/project1/game/GameRunner.java
@@ -1,5 +1,8 @@
 package com.sampson.project1.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class GameRunner {
 
     GamingConsole game;
--- src/main/java/com/sampson/project1/gameSpring/AppGamingSpring.java
+++ src/main/java/com/sampson/project1/gameSpring/AppGamingSpring.java
@@ -2,12 +2,17 @@ package com.sampson.project1.gameSpring;
 
 import com.sampson.project1.game.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("com.sampson.project1.game")
 public class AppGamingSpring {
 
     public static void main(String[] args){
 
-        var context = new AnnotationConfigApplicationContext(GameConfiguration.class);
+        var context = new AnnotationConfigApplicationContext(AppGamingSpring.class);
 
         context.getBean(GameRunner.class).run();
 
