355b71db0318f1eaf452c528aad814573128c78e
fsampson30 <fsampson30@gmail.com>
2023-10-20 07:29:10 -0300

    Renamed launcher class.

--- src/main/java/com/sampson/project1/gameSpring/GamingAppLauncherApplication.java
+++ src/main/java/com/sampson/project1/gameSpring/GamingAppLauncherApplication.java
@@ -1,18 +1,17 @@
 package com.sampson.project1.gameSpring;
 
-import com.sampson.project1.game.*;
+import com.sampson.project1.game.GameRunner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.sampson.project1.game")
-public class AppGamingSpring {
+public class GamingAppLauncherApplication {
 
     public static void main(String[] args){
 
-        var context = new AnnotationConfigApplicationContext(AppGamingSpring.class);
+        var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class);
 
         context.getBean(GameRunner.class).run();
 
