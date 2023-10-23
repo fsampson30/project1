f19579164d58479cbf1f4b8deb1e6076867e891f
fsampson30 <fsampson30@gmail.com>
2023-10-23 19:59:18 -0300

    Using Field Injection.

--- src/main/java/com/sampson/project1/gameSpring/GamingAppLauncherApplication.java
+++ src/main/java/com/sampson/project1/gameSpring/GamingAppLauncherApplication.java
@@ -19,6 +19,8 @@ public class GamingAppLauncherApplication {
 
         Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
+        System.out.println(context.getBean(YourBusinessClass.class));
+
 
     }
 }
--- src/main/java/com/sampson/project1/gameSpring/YourBusinessClass.java
+++ src/main/java/com/sampson/project1/gameSpring/YourBusinessClass.java
@@ -1,7 +1,18 @@
 package com.sampson.project1.gameSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class YourBusinessClass {
+    @Autowired
+    Dependency1 dependency1;
+    @Autowired
+    Dependency2 dependency2;
+
+    public String toString(){
+        return "Using " + dependency1 + " and " + dependency2;
+    }
+
+
 }
