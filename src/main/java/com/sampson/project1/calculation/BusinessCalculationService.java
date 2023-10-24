de6c3c94155953bb4eaeedf02b7df7e8761e0c7f
fsampson30 <fsampson30@gmail.com>
2023-10-24 07:57:13 -0300

    Refactored DataService to be private.

--- src/main/java/com/sampson/project1/calculation/BusinessCalculationService.java
+++ src/main/java/com/sampson/project1/calculation/BusinessCalculationService.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 @Component
 public class BusinessCalculationService {
 
-    DataService dataService;
+    private DataService dataService;
 
     public BusinessCalculationService(@Qualifier("MySQLDataService") DataService dataService) {
         this.dataService = dataService;
