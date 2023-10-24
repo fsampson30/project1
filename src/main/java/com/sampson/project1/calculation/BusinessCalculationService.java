ba62addf4d4ee65b0c00cce6829d53f4308b3eb7
fsampson30 <fsampson30@gmail.com>
2023-10-24 07:35:59 -0300

    Created new classes BusinessCalculationService and MySQLDataService.

---
+++ src/main/java/com/sampson/project1/calculation/BusinessCalculationService.java
@@ -0,0 +1,19 @@
+package com.sampson.project1.calculation;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class BusinessCalculationService {
+
+    DataService dataService;
+
+    public BusinessCalculationService(DataService dataService) {
+        this.dataService = dataService;
+    }
+
+    public int findMax(){
+        return Arrays.stream(dataService.retrieveData()).max().orElse(0);
+    }
+}
---
+++ src/main/java/com/sampson/project1/calculation/MySQLDataService.java
@@ -0,0 +1,9 @@
+package com.sampson.project1.calculation;
+
+public class MySQLDataService implements DataService {
+
+    @Override
+    public int[] retrieveData() {
+        return new int[] {1,2,3,4,5};
+    }
+}
