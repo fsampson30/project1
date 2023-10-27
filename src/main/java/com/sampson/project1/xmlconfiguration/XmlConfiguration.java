35cb8ee95794fb7ce3a4efed8a4cb1b038b9843d
fsampson30 <fsampson30@gmail.com>
2023-10-27 07:36:54 -0300

    Created new XML configuration file.

--- src/main/java/com/sampson/project1/xmlconfiguration/XmlConfiguration.java
+++ src/main/java/com/sampson/project1/xmlconfiguration/XmlConfiguration.java
@@ -3,6 +3,7 @@ package com.sampson.project1.xmlconfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
 
@@ -12,7 +13,7 @@ public class XmlConfiguration {
 
     public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext(XmlConfiguration.class);
+        var context = new ClassPathXmlApplicationContext("contextConfiguration.xml");
 
         Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
