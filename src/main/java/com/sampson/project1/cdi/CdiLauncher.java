8eba1a71813a71b352f96f6fa646fde34a68014a
fsampson30 <fsampson30@gmail.com>
2023-10-27 07:16:04 -0300

    Created new package to Jakarta CDI.

---
+++ src/main/java/com/sampson/project1/cdi/CdiLauncher.java
@@ -0,0 +1,19 @@
+package com.sampson.project1.cdi;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan
+public class CdiLauncher {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(CdiLauncher.class);
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+    }
+}
